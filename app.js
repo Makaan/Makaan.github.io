4f282b9a93a8c19242cc7a76d4f335928cd474f6
Makaan <nestor.figliuolo@gmail.com>
2020-05-09 19:52:59 -0300

    new JSON

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+const randomNumberInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
 const init = async () => {
-  let response = await fetch('/datos.json')
+  let response = await fetch('https://makaan.github.io/datos.json')
   let json = await response.json()
 
+  let preguntas = []
+
+  for(pais of json) {
+    const opciones = []
+    let i = 0
+    while (i < 4) {
+      const random = randomNumberInRange(0, json.length - 1)
+      if(json[random].Capital !== pais.Capital) {
+        opciones.push(json[random].Capital)
+        i++
+      }
+    }
+    preguntas.push({
+      nombreComun: pais.NombreComun,
+      capital: pais.Capital,
+      opciones: opciones
+    })
+  }
+
   //Una vez que ya tenes el json manipulalo como quieras
-  document.getElementById('datos').innerHTML = JSON.stringify(json, null, 2) 
+  document.getElementById('datos').textContent = JSON.stringify(preguntas, undefined, 2);
 }
 
 init()
